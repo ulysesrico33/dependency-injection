01ee5a3317708fd14b07b17c75844d4d2fb63e07
Ulyses Rico Rea <ulyses.rico.2012@gmail.com>
2024-03-03 19:24:36 -0600

    ...

--- projectApi/Program.cs
+++ projectApi/Program.cs
@@ -1,6 +1,11 @@
+using Service;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+//Services injection
+builder.Services.AddSingleton<IThing>();
 
 app.Run();
